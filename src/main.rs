224b6810b40b5dca0defb2f7e20d696b90df8187
Valentin Hubert <valentin.hubert@reseau.eseo.fr>
2023-10-09 10:38:45 +0200

    main avec les starters de gui et observer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
 mod game_loop;
+use macroquad::prelude::*;
 
-fn main() {
-    println!("Hello world");
+#[macroquad::main("Background Image Example", window_conf)]
+async fn main() {
+    let mut show_title = true; // Initial state: afficher la texture du titre
+
+    // Créez un canal pour envoyer des événements de clavier et souris
+    let (sender, receiver) = std::sync::mpsc::channel::<macroquad::input::KeyCode>();
+    let observer = game_loop::keyboard::KeyboardObserver::new(sender);
+    observer.start_observer();
+
+    // Gérer l'affichage graphique
+
+    let graphics_manager = game_loop::gui::GraphicsManager::new().await;
+    let mut graphics_manager = match graphics_manager {
+        Some(game_manager) => game_manager,
+        None => return,
+    };
+
+    println!("Starting loop");
+    loop {
+        clear_background(WHITE);
+
+        if let Ok(key_code) = receiver.recv() {
+            if key_code == KeyCode::Escape {
+                show_title = true;
+            }
+        }
+
+        if show_title {
+            // Dessinez la texture du titre
+            graphics_manager.draw_title();
+
+        } else {
+            // Dessinez la texture d'arrière-plan
+            graphics_manager.draw_background_game();
+        }
+
+        next_frame().await;
+    }
 }
