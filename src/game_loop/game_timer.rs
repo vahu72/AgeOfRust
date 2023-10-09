4b636bbffe43c052b329741ba643c62be517fd97
Louisonn <portault@gmail.com>
2023-10-09 10:36:19 +0200

    ajout du module timer (ca fonctionne)

---
+++ src/game_loop/game_timer.rs
@@ -0,0 +1,49 @@
+use std::time::{Duration, Instant};
+use std::thread;
+use std::sync::{Arc, Mutex};
+
+type Callback = fn();
+
+pub struct GameTimer {
+    interval: Duration,
+    callback: Callback,
+    is_running: Arc<Mutex<bool>>,
+}
+
+impl GameTimer {
+    pub fn game_timer_new(interval_ms: u64, callback: Callback) -> Self {
+        let interval = Duration::from_millis(interval_ms);
+        GameTimer {
+            interval,
+            callback,
+            is_running: Arc::new(Mutex::new(true)),
+        }
+    }
+
+    pub fn game_timer_start(&self) {
+        let interval = self.interval;
+        let callback = self.callback;
+        let is_running = self.is_running.clone();
+
+        thread::spawn(move || {
+            let mut last_time = Instant::now();
+
+            while *is_running.lock().unwrap() {
+                let now = Instant::now();
+                let elapsed = now.duration_since(last_time);
+
+                if elapsed >= interval {
+                    last_time = now;
+                    (callback)(); // Call the callback function
+                }
+
+                // Sleep for a short duration to avoid busy-waiting
+                thread::sleep(Duration::from_millis(10));
+            }
+        });
+    }
+
+    pub fn game_timer_stop(&self) {
+        *self.is_running.lock().unwrap() = false;
+    }
+}
